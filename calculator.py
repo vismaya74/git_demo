2bd046838b4a392bb98dc08abd3305548b554ac2
vismaya74 <vismayanair2504@gmail.com>
2023-01-11 13:13:10 +0530

    final modification

--- calculator.py
+++ calculator.py
@@ -1,7 +1,11 @@
 
-a=float(input('enter first number '))
-b=float(input('enter second number'))
-sum=a+b
-print(sum)
+try:
+    a= float(input("enter the number"))
+    b=float(input("enter the second number"))
+except Exception as e:
+    print(e)
+else:
+    sum=a+b
+    print(sum)
 
 
